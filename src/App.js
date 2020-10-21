6f8721129520f4caa8699e69a8892c83e418304f
helladyo <heladiofog@gmail.com>
2020-10-20 23:52:20 -0500

    Initial components scaffolding. Login functionality. Fake backend server and db. Context creation and wrapping to Login.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,176 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+// import logo from './logo.svg';
+// import './App.css';
+import AddProduct from "./components/AddProduct";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+import Login from "./components/Login";
+// context
+import Context from "./context/Context";
+// utils
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: [],
+    };
+
+    this.routerRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // loads the last user session from the local storage to the state if it exists
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+  // Context methods:
+  // login
+  login = async (email, password) => {
+    const res = await axios
+      .post("http://localhost:3001/login", { email, password })
+      .catch((err) => {
+        return { status: 401, message: "Unauthorized" };
+      });
+
+    if (res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken);
+      const user = {
+        email,
+        token: res.data.accessTokken,
+        accessLevel: email === "admin@example.com" ? 0 : 1,
+      };
+
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  };
+  // logout
+  /**
+   * Logout method clears the user from both state and local storage.
+   * @param {object} e Event object
+   */
+  logout = (e) => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCArt: this.removeFromCArt,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout,
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className="App">
+            <nav
+              className="navbar container"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div className="navbar-brand">
+                <Link to="/products" className="navbar-item">
+                  <b className="navbar-item is-size-4 ">e-commerce</b>
+                </Link>
+                <label
+                  role="button"
+                  className="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </label>
+              </div>
+              <div
+                className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}
+              >
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    {Object.keys(this.state.cart).length}
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
+                ) : (
+                  <Link to="/" onClick={this.logout} className="navbar-item">
+                    Logout
+                  </Link>
+                )}
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
